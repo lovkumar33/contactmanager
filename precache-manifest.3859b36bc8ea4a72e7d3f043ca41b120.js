daf44a2337f3372e6663befc71eb2672d06bfa1d
lovkumar33 <lovkumar33@gmail.com>
2020-05-21 18:36:13 +0530

    Updates

---
+++ precache-manifest.3859b36bc8ea4a72e7d3f043ca41b120.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eedb76d2a191c6dede4c94a53d33e61e",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "4255d0def3bcce06027a",
+    "url": "/contactmanager/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "bbb0fe6a42ab7fa92502",
+    "url": "/contactmanager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "4255d0def3bcce06027a",
+    "url": "/contactmanager/static/js/2.1dc7fc41.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/contactmanager/static/js/2.1dc7fc41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bbb0fe6a42ab7fa92502",
+    "url": "/contactmanager/static/js/main.9d68bc91.chunk.js"
+  },
+  {
+    "revision": "3959f31844f85f026a86",
+    "url": "/contactmanager/static/js/runtime-main.711199b3.js"
+  }
+]);
